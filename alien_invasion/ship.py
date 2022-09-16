3809357d8fcb285bfb7fca6f8d0510376ec38882
`Denis <denisott@gmail.com>
2022-09-16 17:22:32 -0300

    Added bullets

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -22,7 +22,7 @@ class Ship:
 
         # Movement flags
         self.moving_right = False
-        self.moving_left  = False
+        self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flag."""
